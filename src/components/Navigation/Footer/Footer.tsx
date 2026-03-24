df35e5fe7e583f42a4e20622a5d0c271698b4cc8
Ann Kuts <kuutsann@gmail.com>
2026-03-24 16:44:53 +0200

    chore: update gh links

--- src/components/Navigation/Footer/Footer.tsx
+++ src/components/Navigation/Footer/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
       <footer className="footer">
         <p>© 2026 Zvycha. Open Source Project.</p>
         <div className="footer-links">
-          <a href="#" className="link-privacy">Privacy Policy</a>
-          <a href="#" className="link-contribute">How to contribute</a>
+          <a href="https://github.com/AnnKuts/zvycha-app/blob/main/docs/PRIVACY.md" className="link-privacy">Privacy Policy</a>
+          <a href="https://github.com/AnnKuts/zvycha-app/blob/main/docs/CONTRIBUTING.md" className="link-contribute">How to contribute</a>
         </div>
       </footer>
     </div>
--- src/components/Navigation/Header/Header.tsx
+++ src/components/Navigation/Header/Header.tsx
@@ -1,17 +1,39 @@
 import logo from "../../../assets/logos/logo.svg";
 import "./Header.css";
+import {useState} from "react";
+import BetaModal from "../../BetaModal/BetaModal";
+
 const Header = () => {
+  const [isBetaModalOpen, setIsBetaModalOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
+
       <nav>
-        <a href="#">Join demo</a>
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            setIsBetaModalOpen(true);
+          }}
+        >
+          Join demo
+        </a>
+
         <a href="#how-it-works">Guide</a>
         <a href="#features">Features</a>
-        <a href="#">Contribute</a>
+        <a href="https://github.com/AnnKuts/zvycha-app/blob/main/docs/CONTRIBUTING.md">
+          Contribute
+        </a>
       </nav>
+
+      <BetaModal
+        isOpen={isBetaModalOpen}
+        onClose={() => setIsBetaModalOpen(false)}
+      />
     </header>
   );
 };
